15a8b3d90a3c8c0626d8e3eebcafa0bf31fd6a50
juykyung.kim <jukyung.kim@nhn.com>
2018-08-13 19:14:29 +0900

    인쇄가능한 샘플 페이지 구성

---
+++ src/components/Memo.jsx
@@ -0,0 +1,41 @@
+/*
+*
+* Memo
+*
+*/
+import React, {Component, Fragment} from "react";
+import PropTypes from "prop-types";
+
+
+export class Memo extends Component {
+	constructor(props) {
+		super(props);
+
+		const arr = [];
+		for (let i = 0; i < 16; i++) {
+			arr[i] = i;
+		}
+		this.state = {arr};
+	}
+
+	render() {
+		return (
+			<div className="memo">
+				<div style={{borderBottom: "1px solid #000", height: "20px"}}>Memo</div>
+				<ul>
+					{this.state.arr.map(() => (
+						<li className="memo_li">
+							<input type="checkbox"/>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+}
+
+Memo.defaultProps = {};
+
+Memo.propTypes = {};
+
+export default Memo;
--- src/components/MonthSheet.jsx
+++ src/components/MonthSheet.jsx
@@ -5,6 +5,7 @@
 */
 import React, {Component, Fragment} from "react";
 import PropTypes from "prop-types";
+import Memo from "./Memo";
 
 
 export class MonthSheet extends Component {
@@ -19,10 +20,11 @@ export class MonthSheet extends Component {
 	componentDidMount() {
 		const frames = window.document.getElementsByClassName("frame");
 		const el = frames[this.props.idx];
-		el.style.position = "absolute";
+		el.style.position = "relative";
 		el.style.width = "900px";
-		el.style.height = "740px";
+		el.style.height = `${700}px`;
 		el.style.top = "1cm";
+		// el.style.top = "-110px";
 		el.style.left = "1cm";
 		// el.style.border = "1px solid black";
 		// window.print();
@@ -31,9 +33,10 @@ export class MonthSheet extends Component {
 	render() {
 		return (
 			<div className="page">
-				<span className="month_str">{this.mon_type[this.props.idx]} 2018</span>
-				<div className="month">{`${this.props.idx + 1}`}</div>
-				<div  dangerouslySetInnerHTML={{__html: this.props.htmlData}} />
+				<span className="test"><span className="month_str">{this.mon_type[this.props.idx]}. <span style={{fontWeight: "normal"}}>2018</span></span></span>
+				<span className="test"><div className="month">{`${this.props.idx + 1}`}</div></span>
+				<div className="end"  dangerouslySetInnerHTML={{__html: this.props.htmlData}} />
+				<Memo/>
 			</div>
 		);
 	}
